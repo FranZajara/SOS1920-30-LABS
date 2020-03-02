2b54a3f1fa7043b5454edb706b706625ca295574
FranZajara <franzajaragomez@gmail.com>
2020-03-02 16:45:17 +0000

    first working version

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-console.log("hello");
-const express = require("express");
-var app = express();
-app.get("/cool",(request, response) => {
-	response.send("<html>"+cool()+"</html>");
-		});
-app.listen(80);
+const cool = require("cool-ascii-faces");
 
+
+
+console.log(cool());
