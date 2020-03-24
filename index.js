993bb6fc7b8a82c615116b1735aa28c84cc684b5
FranZajara <franzajaragomez@gmail.com>
2020-03-24 16:59:33 +0000

    make it better

--- index.js
+++ index.js
@@ -15,30 +15,130 @@ app.get("/cool",(request,response) => {
 
 
 
-var contacts = [
+var sugarconsume = [
 	{
-		name: "peter",
-		phone: 123456
+		place: "Europe",
+		sugarconsume: 18800
 		
 	},
 	{
-		name: "pablo",
-		phone: 789456
+		place: "China",
+		sugarconsume: 17500
+		
+	},
+	{
+		place: "India",
+		sugarconsume: 28000
+		
+	},
+	{
+		place: "Turkey",
+		sugarconsume: 2300
+		
+	},
+	{
+		place: "Ukraine",
+		sugarconsume: 1445
 		
 	}
 ];
 
 const BASE_API_URL = "/api/v1";
 
+//GET CONTACTS
 
-app.get(BASE_API_URL+"/contacts",(req,res) => {
-	res.send(JSON.stringify(contacts, null, 2));
-});
-app.post(BASE_API_URL+"/contacts",(req,res) => {
-	contacts.push(req.body);
-	res.sendStatus(201, "CREATED");
+app.get(BASE_API_URL+"/sugarconsume",(req,res) => {
+	res.send(JSON.stringify(sugarconsume, null, 2));
+	console.log("Data sent:"+JSON.stringify(sugarconsume,null,2));
 });
 
+//POST CONTACTS
+
+app.post(BASE_API_URL+"/sugarconsume",(req,res) => {
+	var newSugarConsume = req.body;
+	if((newSugarConsume == "") || (newSugarConsume.place == null)){
+			res.sendStatus(400, "BAD REQUEST(no name provided)");
+	}
+	else{
+		sugarconsume.push(newSugarConsume);
+		res.sendStatus(201, "CREATED");
+	}
+});
+
+
+
+
+
+//GET CONTACT/XXX
+app.get(BASE_API_URL+"/sugarconsume/:place",(req,res) => {
+	var place = req.params.place;
+	var filteredSugarConsume = sugarconsume.filter((c) =>{
+		return (c.place == place);
+	});
+	if(filteredSugarConsume.length >= 1){
+		res.send(filteredSugarConsume[0]);
+	}else{
+		res.sendStatus(404, "CONTACT NOT FOUND");
+	}
+		
+	
+	
+//DELETE CONTACTS
+	app.delete(BASE_API_URL+"/sugarconsume",(req,res) => {
+		if(sugarconsume.length == 0){
+			res.sendStatus(404, "SUGARCONSUME NOT FOUND");
+		}else{
+			sugarconsume = "";
+			res.sendStatus(200,"OK")
+		}
+	});
+	
+	
+});
+//PUT CONTACT/XXX
+app.put(BASE_API_URL+"/sugarconsume/:place",(req,res) => {
+	var place = req.params.place;
+	if(!place){
+		res.sendStatus(404);
+	}
+	else{
+		if(place == ""){
+			res.sendStatus(400, "PLACE DOES NOT EXIST")
+		}else{
+			place == req.body.place;
+			res.sendStatus(200, "PLACE PUT OK")
+		}
+	}
+	
+	
+});
+//DELETE CONTACT/XXX
+
+app.delete(BASE_API_URL+"/sugarconsume/:place",(req,res) => {
+	var place = req.params.place;
+	var filteredSugarConsume = sugarconsume.filter((c) =>{
+		return (c.place != place);
+	});
+	if(filteredSugarConsume.length < sugarconsume.length){
+		sugarconsume = filteredSugarConsume;
+		res.sendStatus(200, "OK");
+		
+	}else{
+		res.sendStatus(404, "SUGARCONSUME PLACE NOT FOUND");
+	}
+});
+
+//POST CONTACT/XXX
+
+app.post(BASE_API_URL+"/sugarconsume/:place",(req,res) => {
+	
+		res.sendStatus(405, "METHOD NOT ALLOWED");
+	});
+
+//PUT CONTACT
+app.put(BASE_API_URL+"/sugarconsume",(req,res) => {
+	res.sendStatus(405, "METHOD NOT ALLOWED");
+});
 
 app.listen(port, () => {
 	console.log("Server ready");
